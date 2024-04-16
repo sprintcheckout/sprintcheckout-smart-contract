22512d750c59f9f64efdd21aa79dfa2f6760e966
JSG <senentjoan@gmail.com>
2024-04-16 23:02:51 +0200

    Update SprintCheckout.sol
    msg.sender

--- SprintCheckout.sol
+++ SprintCheckout.sol
@@ -4,13 +4,13 @@ import "hardhat/console.sol";
 
 contract SprintCheckout {
 
-    function transferFrom(address selectedToken, address customer, address merchant, address sprintcheckout, uint amount, uint spcFee) public {
+    function transferFrom(address selectedToken, msg.sender, address merchant, address sprintcheckout, uint amount, uint spcFee) public {
         IERC20 token = IERC20(selectedToken);
         require(
-            token.allowance(customer, address(this)) >= amount + spcFee, "Token allowance too low"
+            token.allowance(msg.sender, address(this)) >= amount + spcFee, "Token allowance too low"
         );
-        _safeTransferFrom(token, customer, sprintcheckout, spcFee);
-        _safeTransferFrom(token, customer, merchant, amount);
+        _safeTransferFrom(token, msg.sender, sprintcheckout, spcFee);
+        _safeTransferFrom(token, msg.sender, merchant, amount);
     }
 
     function _safeTransferFrom(
